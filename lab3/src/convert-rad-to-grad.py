84b5816763b834add04867fb5c5a59d0e2932641
worthant <b_dvorkin@niuitmo.ru>
2024-10-23 11:04:01 +0300

    :hammer: feat(lab1/p1)!: Calculations & diagrams

---
+++ lab3/src/convert-rad-to-grad.py
@@ -0,0 +1,11 @@
+import math
+
+numerator = -111.96
+denominator = 30
+
+# Calculate arctangent (arcctg) in radians
+arc_tan_radians = math.atan(numerator / denominator)
+
+# Convert radians to degrees
+arc_tan_degrees = math.degrees(arc_tan_radians)
+print(arc_tan_degrees)
